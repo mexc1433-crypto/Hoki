3fc922113f1b820e591687eb5feba0ac83a88e61
Hassanjj <mexc1433@gmail.com>
2025-11-02 11:28:06 +0200

    Update Info.py

--- Info.py
+++ Info.py
@@ -1,12 +1,12 @@
 #Twilio
 
-account_sid = ''
-auth_token = ''
-twilionumber = ''
-twiliosmsnumber = ''
+account_sid = 'ACaf63cc09cd50b3b06e1c2f9b06bb48d0'
+auth_token = '40031aa734baaffb41fda88835895467'
+twilionumber = '+18028022056'
+twiliosmsnumber = '+18028022056'
 
 #Telegram Bot Token
-API_TOKEN = "" 
+API_TOKEN = "7101638922:AAFTV4x1CFrldsIGP335qGzhoTdBkXW8jSE" 
 
 #Host URL
 callurl = 'https://03f3-196-191-60-243.ngrok.io' 
